f34a60cdf630892d9897d3af37cb3eaceea05eb6
Raigon Kunnath Augustin <raigonka@gmail.com>
2023-08-04 10:46:23 +0200

    moved the config file and added dataset split ration + load dataset and split

--- app/src/data_utils.py
+++ app/src/data_utils.py
@@ -1,12 +1,38 @@
+import pandas as pd
 from datasets import load_dataset
 import torch
 from torch.utils.data import TensorDataset, DataLoader
+from sklearn.model_selection import train_test_split
+import configparser
 
 
 def load_data(dataset_name):
-    """Loads the dataset from Hugging Face dataset library and returns the train, test and validation splits"""
+    """Loads the dataset from Hugging Face hub using dataset library and returns the train, test and validation dataFrames"""
+
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
     data = load_dataset(dataset_name)
-    return data['train'], data['validation'], data['test']
+
+    if dataset_name == 'imdb':      # For IMDB dataset, I'm using a custom split instead of the default 25k for test and train
+
+        combined_IMDB_frame = pd.concat([data['train'].to_pandas(), data['test'].to_pandas()])  # Combines the train and test splits
+        combined_IMDB_frame = combined_IMDB_frame.sample(frac=1, random_state=45)
+
+        train_ratio = config.getfloat('IMDB', 'train_ratio')    # Loading the train, test and val split ratios from config file
+        test_ratio = config.getfloat('IMDB', 'test_ratio')
+        val_ratio = config.getfloat('IMDB', 'val_ratio')
+
+        # Creating the train, test and validation data frames
+
+        train_data, temp_data = train_test_split(combined_IMDB_frame, test_size=1 - train_ratio, random_state=45)
+        val_data, test_data = train_test_split(temp_data, test_size=test_ratio / (test_ratio + val_ratio), random_state=45)
+
+        return train_data, val_data, test_data
+
+    elif dataset_name == 'rotten_tomatoes':     # For rotten_tomatoes dataset, I'm using the default split
+
+        return data['train'].to_pandas(), data['validation'].to_pandas(), data['test'].to_pandas()
 
 
 def getDataLoader(frame, tokenizer, batch_size):
